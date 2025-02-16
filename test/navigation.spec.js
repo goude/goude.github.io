3a6577fa820a10c62ae9bc5e1e81b338c0d8bb28
Daniel Goude <daniel@goude.se>
2025-02-16 11:53:10 +0100

    increase timeouts

--- test/navigation.spec.js
+++ test/navigation.spec.js
@@ -6,15 +6,21 @@ test("Site navigation and keyword validation", async ({ page }) => {
 
   // Visit the homepage and check for the keyword "Entry Point"
   await page.goto(baseUrl);
-  await expect(page.locator("text=Entry Point")).toBeVisible();
+  await expect(page.locator("text=Entry Point")).toBeVisible({
+    timeout: 10000,
+  });
 
   // Click the link with text "it" and check that the page contains "water in their face"
   await page.click('text="it"');
   await page.waitForLoadState("networkidle");
-  await expect(page.locator("text=water in their face")).toBeVisible();
+  await expect(page.locator("text=water in their face")).toBeVisible({
+    timeout: 10000,
+  });
 
   // Click the link "finding-humanity-what-we-do-in-the-shadows" and check for "At its heart"
   await page.click("text=finding-humanity-what-we-do-in-the-shadows");
   await page.waitForLoadState("networkidle");
-  await expect(page.locator("text=At its heart")).toBeVisible();
+  await expect(page.locator("text=At its heart")).toBeVisible({
+    timeout: 10000,
+  });
 });
