5b3fcce5c37470fff2496f4aefbf73640eef4d62
Daniel Goude <daniel@goude.se>
2025-02-16 12:07:11 +0100

    get rid of warnings

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 .PHONY: clean test
 
 clean:
-	rm -rf dist/
+	rm -rf dist/ test-results/
 
 test:
 	node test/fileUniqueness.js
--- test/fileUniqueness.js
+++ test/fileUniqueness.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-const fs = require("fs");
-const path = require("path");
+import fs from "fs";
+import path from "path";
 
-const srcDir = path.join(__dirname, "..", "src");
+const srcDir = path.join(process.cwd(), "src");
 // Define a whitelist of filenames that are allowed to have duplicates.
 const whitelist = new Set(["index.astro"]);
 
