f48a4b33a2a86fbd682dd9ba8c24361f7713fa0b
Daniel Goude <daniel@goude.se>
2025-03-19 18:58:15 +0100

    add countdown

--- test/navigation.spec.js
+++ test/navigation.spec.js
@@ -4,15 +4,9 @@ test("Site navigation and keyword validation", async ({ page }) => {
   const baseUrl = "http://localhost:4321";
 
   await page.goto(baseUrl);
-  await expect(page.locator("text=Entry Point")).toBeVisible();
+  await expect(page.locator("text=Hello There")).toBeVisible();
 
-  // Click the link with text "it" and check that the page contains "water in their face"
-  await page.click('text="it"');
+  await page.click("text=about");
   await page.waitForLoadState("networkidle");
-  await expect(page.locator("text=water in their face")).toBeVisible({ timeout: 20000 });
-
-  // Click the link "finding-humanity-what-we-do-in-the-shadows" and check for "At its heart"
-  await page.click("text=finding-humanity-what-we-do-in-the-shadows");
-  await page.waitForLoadState("networkidle");
-  await expect(page.locator("text=At its heart")).toBeVisible({ timeout: 20000 });
+  await expect(page.locator("text=dig")).toBeVisible();
 });
