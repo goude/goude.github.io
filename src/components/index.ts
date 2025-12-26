987644ad51a3a77d346cf59563124f9f78dc0fb7
Daniel Goude <daniel@goude.se>
2025-12-26 21:19:24 +0100

    minor edit/housekeeping activity

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,4 @@
 export { default as BaseHead } from "./BaseHead.astro";
 export { default as Header } from "./Header.astro";
 export { default as Footer } from "./Footer.astro";
-export { default as Logo } from "./Logo.astro";
-export { default as Nav } from "./Nav.astro";
-export { default as ThemeToggle } from "./ThemeToggle.astro";
 export { default as Md } from "./Md.astro";
